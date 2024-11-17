ada5a7830bdb44513226ca90983c3ecabb7fdc5f
devlimit <devlimit12@gmail.com>
2024-11-17 17:49:49 +0900

    [완전탐색]- 모의고사

--- src/main/java/jh/programmers/search/day2/P241117_3_H.java
+++ src/main/java/jh/programmers/search/day2/P241117_3_H.java
@@ -1,47 +1,57 @@
 package jh.programmers.search.day2;
 
 
+import jh.programmers.math.day1.P241113_H;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 문제:모의고사
  * url: https://school.programmers.co.kr/learn/courses/30/lessons/42840
  * 풀이자 : 신혜림
- * 풀이시간: 1 시간 30분 --- 진짜 모르겠음
+ * 풀이시간: 1 시간 40분
  */
 public class P241117_3_H {
 
     public int[] solution(int[] answers) {
-        int[] answer = new int [3];
         int []  numberOne =  {1,2,3,4,5}; //5개
         int [] numberTwo = {2, 1, 2, 3, 2, 4, 2, 5, 2}; //8개
         int [] numberThree = { 3, 3, 1, 1, 2, 2, 4, 4, 5, 5};// 10개
-
-        int numberOneCount = 0;
-        int numberTwoCount = 0;
-        int numberThreeCount = 0;
+        int [] count = {0,0,0};
         //for문을 하나씩돌아서 맞은 개수를 찾아야하나?
         for(int i = 0; i<answers.length; i++) {//answer의 길이만큼
-            //나머지사용해서 계산하면될것같은데
-            int  num = numberOne.length %5;
-            int  numTwo = numberTwo.length %9;
-            int  numThree = numberThree.length %10;
+            int  num = i %5;
+            int  numTwo = i %8;
+            int  numThree = i %10;
             if(numberOne[num] == answers[i]){
-                numberOneCount++;
+                count[0]++;
             }
             if(numberTwo[numTwo] == answers[i]){
-                numberTwoCount++;
+                count[1]++;
             }
             if(numberThree[numThree] == answers[i]){
-                numberThreeCount++;
+                count[2]++;
             }
         }
         //최대값을 구해서 값 넣어주기
-        if(numberOneCount> numberTwoCount) {
-            if(numberOneCount > numberThreeCount){
-               // answer.add(1);
-            }
+        int max = Math.max(count[0],Math.max(count[1],count[2]));
+        //누가 최대값인지 찾기
+        List<Integer> maxPerson = new ArrayList<>();
+        for(int i = 0; i<count.length; i++){
+            if(max == count[i]) maxPerson.add(i+1);
         }
-
-
+        int[] answer =new int[maxPerson.size()];
+        for(int i=0; i<maxPerson.size(); i++){
+            answer[i] = maxPerson.get(i);
+        }
         return answer;
+    }
+    public static void main(String[] args) {
+        P241117_3_H p241117_3_h = new P241117_3_H();
+        int [] arr = {1,2,3,4,5};
+        int [] arr2 = {1,3,2,4,2};
+        System.out.println(p241117_3_h.solution(arr));
+        System.out.println(p241117_3_h.solution(arr2));
     }
 }
